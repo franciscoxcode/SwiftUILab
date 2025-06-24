b0dc0f38cf48e87be1910187d5748592b8992345
Francisco Casillas <chiccasillas@gmail.com>
2025-06-24 17:29:24 -0600

    Initial Commit

---
+++ SwiftUILab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILab
+//
+//  Created by Francisco Jean on 24/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILab/SwiftUILabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILabApp.swift
+//  SwiftUILab
+//
+//  Created by Francisco Jean on 24/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
